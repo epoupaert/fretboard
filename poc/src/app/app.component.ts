d635a930627a142327676e43029a620f1c7d6455
Eric Poupaert <epoupaert@gmail.com>
2020-09-29 16:07:32 +0200

    added SVG

--- poc/src/app/app.component.ts
+++ poc/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SVG } from '@svgdotjs/svg.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'poc';
+
+  ngOnInit(): void {
+    const draw = SVG().addTo('#canvas').size(400, 400);
+    const rect = draw.rect(100, 100);
+  }
+
 }
